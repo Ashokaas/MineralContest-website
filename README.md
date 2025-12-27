39de61a59c4a3c51d53292772cfd51ff3f36ff97
Ashokaas <antotosun@gmail.com>
2025-12-27 12:19:15 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+the website for the Mineral Contest, wowzers
+https://viciscat.github.io/MineralContest/
---
+++ nav_scroll.js
@@ -0,0 +1,19 @@
+function scrollToTop() {
+    document.documentElement.scrollTo({ top: 0, behavior: 'smooth' });
+
+}
+
+function scrollToText() {
+    document.getElementById("rules").scrollIntoView({ behavior: 'smooth' });
+
+}
+
+function scrollToDownload() {
+    document.getElementById("download").scrollIntoView({ behavior: 'smooth' });
+
+}
+
+function scrollToAbout() {
+    document.getElementById("about").scrollIntoView({ behavior: 'smooth' });
+
+}
